5bc79d79559a4794e395a84e0a305287baf22708
Richard Dupont <rdupont@rheinmetall.ca>
2021-03-26 13:56:47 -0400

    Confident that 0 dividedBy anything gives zero.

--- 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/DivideFractionsTest.java
+++ 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/DivideFractionsTest.java
@@ -3,11 +3,20 @@ package ca.jbrains.math.test;
 import ca.jbrains.math.Fraction;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DivideFractionsTest
 {
     @Test(expected = RuntimeException.class)
     public void dividingByZeroThrows()
     {
         new Fraction(1).dividedBy(new Fraction(0));
+    }
+
+    @Test
+    public void zeroDividedByAnythingEqualsZero()
+    {
+        assertEquals(new Fraction(0), new Fraction(0).dividedBy(new Fraction(1)));
+        assertEquals(new Fraction(0), new Fraction(0).dividedBy(new Fraction(1, 2)));
     }
 }
