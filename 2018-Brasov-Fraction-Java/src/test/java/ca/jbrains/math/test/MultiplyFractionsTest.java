1d934b5542fbb7317489db4efdd0af0061a2804c
Richard Dupont <rdupont@rheinmetall.ca>
2021-03-26 13:37:35 -0400

    Confident that multiplying negative fractions work.

--- 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/MultiplyFractionsTest.java
+++ 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/MultiplyFractionsTest.java
@@ -25,4 +25,16 @@ public class MultiplyFractionsTest
     {
         assertEquals(new Fraction(1, 4), new Fraction(1, 2).times(new Fraction(1, 2)));
     }
+
+    @Test
+    public void onePositiveAndOneNegative()
+    {
+        assertEquals(new Fraction(-3, 16), new Fraction(1, 4).times(new Fraction(-3, 4)));
+    }
+
+    @Test
+    public void negativeFractions()
+    {
+        assertEquals(new Fraction(6, 25), new Fraction(-2, 5).times(new Fraction(-3, 5)));
+    }
 }
