dde7bc43cb8ade4781da98816657272ab7106c5b
Richard Dupont <rdupont@rheinmetall.ca>
2021-03-26 13:33:09 -0400

    Confident that multiplying whole value against fraction works,

--- 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/MultiplyFractionsTest.java
+++ 2018-Brasov-Fraction-Java/src/test/java/ca/jbrains/math/test/MultiplyFractionsTest.java
@@ -12,4 +12,11 @@ public class MultiplyFractionsTest
     {
         assertEquals(new Fraction(0), new Fraction(1).times(new Fraction(0)));
     }
+
+    @Test
+    public void wholeAgainstFraction()
+    {
+        assertEquals(new Fraction(1, 2), new Fraction(1).times(new Fraction(1, 2)));
+        assertEquals(new Fraction(-1, 2), new Fraction(1).times(new Fraction(-1, 2)));
+    }
 }
