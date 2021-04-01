669cd98f3519c08667926539a7d518543c01afab
Rdu-Rheinmetall <81766629+Rdu-Rheinmetall@users.noreply.github.com>
2021-04-01 12:28:11 -0400

    Updated .gitignore and added DataOutputter interface.

--- untitled/src/main/java/ca/rheinmetall/pos/BarcodeProcessor.java
+++ untitled/src/main/java/ca/rheinmetall/pos/BarcodeProcessor.java
@@ -1,9 +1,18 @@
 package ca.rheinmetall.pos;
 
+import ca.rheinmetall.pos.output.DataOutputter;
+
 public class BarcodeProcessor
 {
+    private final DataOutputter _dataOutputter;
+
+    public BarcodeProcessor(final DataOutputter dataOutputter)
+    {
+        _dataOutputter = dataOutputter;
+    }
+
     public void onBarcode(final String barcode)
     {
-        
+        _dataOutputter.output("");
     }
 }
--- untitled/src/test/java/ca/rheinmetall/pos/BarcodeProcessorTest.java
+++ untitled/src/test/java/ca/rheinmetall/pos/BarcodeProcessorTest.java
@@ -1,13 +1,26 @@
 package ca.rheinmetall.pos;
 
+import ca.rheinmetall.pos.output.DataOutputter;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 public class BarcodeProcessorTest
 {
+    private final DataOutputter _mockDataOutputter = mock(DataOutputter.class);
+    private BarcodeProcessor _barcodeProcessor;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        _barcodeProcessor = new BarcodeProcessor(_mockDataOutputter);
+    }
+
     @Test
     public void onEmptyBarcode()
     {
-        final BarcodeProcessor barcodeProcessor = new BarcodeProcessor();
-        barcodeProcessor.onBarcode("");
+        _barcodeProcessor.onBarcode("");
+        verify(_mockDataOutputter).output("");
     }
 }
